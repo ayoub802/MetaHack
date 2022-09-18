74fade4a1c9c37109838e4b316f2200d4ed37c9c
ayoub802 <71781766+ayoub802@users.noreply.github.com>
2022-09-18 16:52:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,33 +35,6 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-  /*==================== Spotlight Effect ====================*/
-  const spotlightSize = 200;
-
-  document.addEventListener('mousemove', function (event) {
-      if (window.event) { // IE fix
-          event = window.event;
-      }
-      // Get the coordinates of the title
-      let titleRect = document.querySelector('.home_title').getBoundingClientRect();
-     
-      // Grab the mouse's X-position
-      let mouseX = event.clientX;
-    
-    // Position spotlight x coordinate based on mouse x, center based on width of spotlight, take into account element x offset
-      let spotlightX = mouseX - (spotlightSize / 2) - titleRect.left;
-    
-      // Grab the mouse's Y position
-      let mouseY = event.clientY;
-    
-    // Position spotlight y coordinate based on mouse y, center based on width of spotlight, take into account element y offset
-    let spotlightY = mouseY  - (spotlightSize / 2) - titleRect.top;
-      
-      // Set x and y position of spotlight
-      const element = document.querySelector('.home_title');
-      element.style.backgroundPosition = spotlightX + 'px ' + spotlightY + 'px';
-  }, false);
-  
 
 
 
